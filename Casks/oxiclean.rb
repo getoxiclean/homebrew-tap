6f2471440e28f61dcfa9517f23f4860378ee3f87
AmenOphis1er <amouzou@gmail.com>
2026-02-26 09:12:43 -0500

    Update cask SHA256 for notarized DMG

--- Casks/oxiclean.rb
+++ Casks/oxiclean.rb
@@ -1,6 +1,6 @@
 cask "oxiclean" do
   version "0.2.0"
-  sha256 "f319018a7c2481468ede37f711d4abffca569156fb2d8a49cd6242ddab6617f4"
+  sha256 "bd84fd6011cdff99c3d1ad51e7866a07edc57f6247b72389c574be258fbd9ff5"
 
   url "https://github.com/getoxiclean/homebrew-tap/releases/download/v0.2.0/OxiCleanApp.dmg"
   name "OxiClean"
